753a18e261444d082d97620d5aa7d918f77dfadc
Daniel Adesanya <4x3l3r8@gmail.com>
2025-01-10 11:43:01 +0100

    mobile view correction

--- src/components/Calendar/DropColumn.tsx
+++ src/components/Calendar/DropColumn.tsx
@@ -8,66 +8,79 @@ import { useMoveTaskMutation } from "@/redux/services/task.api";
 import { toast } from "../shared";
 
 interface DropColumnProps {
-    title: taskStatus;
-    tasks: Task[]
+  title: taskStatus;
+  tasks: Task[];
 }
 
 export const DropColumn = ({ title, tasks }: DropColumnProps) => {
-    const bg = useColorModeValue("#F5F7F9", "#7D8996")
-    const [moveTask] = useMoveTaskMutation()
-    const { isOpen: addIsOpen, onClose: addOnClose, onOpen: addOnOpen } = useDisclosure()
+  const bg = useColorModeValue("#F5F7F9", "#7D8996");
+  const [moveTask] = useMoveTaskMutation();
+  const { isOpen: addIsOpen, onClose: addOnClose, onOpen: addOnOpen } = useDisclosure();
 
-    const handleMove = (id: number) => {
-        moveTask({ newStatus: title, taskId: id }).unwrap().then(() => {
-            toast({
-                status: "success",
-                title: "Operation Successful",
-                description: "Task moved successfully"
-            })
-        })
-    }
+  const handleMove = (id: number) => {
+    moveTask({ newStatus: title, taskId: id })
+      .unwrap()
+      .then(() => {
+        toast({
+          status: "success",
+          title: "Operation Successful",
+          description: "Task moved successfully",
+        });
+      });
+  };
 
-    const [{ isOver }, drop] = useDrop(() => ({
-        accept: "Task",
-        drop: (dragItem: Task) => {
-            handleMove(dragItem.id)
-        },
-        collect: (monitor) => ({
-            isOver: !!monitor.isOver()
-        }),
-    }))
+  const [{ isOver }, drop] = useDrop(() => ({
+    accept: "Task",
+    drop: (dragItem: Task) => {
+      handleMove(dragItem.id);
+    },
+    collect: (monitor) => ({
+      isOver: !!monitor.isOver(),
+    }),
+  }));
 
+  return (
+    <>
+      <Box w={{ md: "30.33%" }} bg={bg} rounded={"lg"} p={3} minW={"20rem"} pos={"relative"} ref={drop}>
+        <HStack>
+          <Heading fontSize={"xl"} fontWeight={"400"} color={"gray.400"}>
+            {title}
+          </Heading>
+          <Tag colorScheme="blackAlpha">{tasks.length}</Tag>
 
-    return (
-        <>
-            <Box w={{ md: "30.33%" }} bg={bg} rounded={"lg"} p={3} minW={"13rem"} pos={"relative"} ref={drop}>
-                <HStack>
-                    <Heading fontSize={"xl"} fontWeight={"400"} color={"gray.400"}>{title}</Heading>
-                    <Tag colorScheme="blackAlpha">{tasks.length}</Tag>
+          <IconButton onClick={addOnOpen} aria-label="add new todo item" variant={"ghost"} colorScheme="gray" icon={<PiPlus />} ml="auto" />
+        </HStack>
 
-                    <IconButton onClick={addOnOpen} aria-label="add new todo item" variant={"ghost"} colorScheme="gray" icon={<PiPlus />} ml="auto" />
-                </HStack>
-
-                <Stack gap={3}>
-                    {/* {new Array(3).fill("").map((_, i) => {
+        <Stack gap={3}>
+          {/* {new Array(3).fill("").map((_, i) => {
                         return <TaskCard key={i} />
                     })} */}
-                    {tasks.map((task, i) => {
-                        return <TaskCard task={task} key={i} />
-                    })}
-                    {tasks.length < 1 &&
-                        <Center rounded={"md"} mt={3} p={4} border="1px dashed" borderColor={"gray.300"}>
-                            <Text color={"secondary.200"}>No tasks</Text>
-                        </Center>
-                    }
-                </Stack>
-                {isOver &&
-                    <Center border={"1px dashed"} borderColor={"primary.400"} opacity={0.8} bg={"primary.100"} pos={"absolute"} w={"94%"} h={"80%"} top={16} rounded={"lg"}>
-                        <Text>Drop Task here</Text>
-                    </Center>
-                }
-            </Box>
-            <AddTaskModal onClose={addOnClose} isOpen={addIsOpen} />
-        </>
-    )
-}
+          {tasks.map((task, i) => {
+            return <TaskCard task={task} key={i} />;
+          })}
+          {tasks.length < 1 && (
+            <Center rounded={"md"} mt={3} p={4} border="1px dashed" borderColor={"gray.300"}>
+              <Text color={"secondary.200"}>No tasks</Text>
+            </Center>
+          )}
+        </Stack>
+        {isOver && (
+          <Center
+            border={"1px dashed"}
+            borderColor={"primary.400"}
+            opacity={0.8}
+            bg={"primary.100"}
+            pos={"absolute"}
+            w={"94%"}
+            h={"80%"}
+            top={16}
+            rounded={"lg"}
+          >
+            <Text>Drop Task here</Text>
+          </Center>
+        )}
+      </Box>
+      <AddTaskModal onClose={addOnClose} isOpen={addIsOpen} />
+    </>
+  );
+};
--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,52 +1,55 @@
-import { DropColumn, Search } from "@/components/Calendar"
-import { Task } from "@/components/Calendar/types"
-import { useGetTasksQuery } from "@/redux/services/task.api"
-import { searchTasks } from "@/utils/helpers"
-import { Box, ButtonGroup, Flex, Heading, HStack, Icon, IconButton, Skeleton } from "@chakra-ui/react"
-import { useMemo, useState } from "react"
-import { DndProvider } from "react-dnd"
-import { HTML5Backend } from "react-dnd-html5-backend"
-import { FiArrowLeft, FiArrowRight } from "react-icons/fi"
+import { DropColumn, Search } from "@/components/Calendar";
+import { Task } from "@/components/Calendar/types";
+import { useGetTasksQuery } from "@/redux/services/task.api";
+import { searchTasks } from "@/utils/helpers";
+import { Box, ButtonGroup, Flex, Heading, HStack, Icon, IconButton, Skeleton } from "@chakra-ui/react";
+import { useMemo, useState } from "react";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { FiArrowLeft, FiArrowRight } from "react-icons/fi";
 
 export const Calendar = () => {
-    const { data: tasks, isLoading } = useGetTasksQuery()
+  const { data: tasks, isLoading } = useGetTasksQuery();
 
-    const [searchValue, setSearchValue] = useState("")
+  const [searchValue, setSearchValue] = useState("");
 
-    const filteredTasks = useMemo(() => {
-        return searchTasks(tasks as Task[], searchValue);
-    }, [tasks, searchValue]);
+  const filteredTasks = useMemo(() => {
+    return searchTasks(tasks as Task[], searchValue);
+  }, [tasks, searchValue]);
 
-    const todoTasks = filteredTasks?.filter(task => task.status === 'To do') || []
-    const inProgressTasks = filteredTasks?.filter(task => task.status === 'In progress') || []
-    const completedTasks = filteredTasks?.filter(task => task.status === 'Completed') || []
+  const todoTasks = filteredTasks?.filter((task) => task.status === "To do") || [];
+  const inProgressTasks = filteredTasks?.filter((task) => task.status === "In progress") || [];
+  const completedTasks = filteredTasks?.filter((task) => task.status === "Completed") || [];
 
-    return (
-        <Box w={"full"}>
-            <HStack w={"full"} flexDir={{ base: "column", md: "row" }}>
-                <Flex w={"full"} gap={6} flex={3} justifyContent={{ base: "space-between", md: "start" }} >
-                    <Heading fontSize={{ base: "2xl", md: "3xl" }}>2 August 2023</Heading>
-                    <ButtonGroup gap={3} variant={"outline"} colorScheme="blackAlpha">
-                        <IconButton aria-label="previous day" rounded={"full"} size={{ base: "sm", md: "md" }} icon={<Icon as={FiArrowLeft} color={"black"} />} />
-                        <IconButton aria-label="next day" rounded={"full"} size={{ base: "sm", md: "md" }} icon={<Icon as={FiArrowRight} color={"black"} />} />
-                    </ButtonGroup>
-                </Flex>
+  return (
+    <Box w={"full"}>
+      <HStack w={"full"} flexDir={{ base: "column", md: "row" }}>
+        <Flex w={"full"} gap={6} flex={3} justifyContent={{ base: "space-between", md: "start" }}>
+          <Heading fontSize={{ base: "2xl", md: "3xl" }}>2 August 2023</Heading>
+          <ButtonGroup gap={3} variant={"outline"} colorScheme="blackAlpha">
+            <IconButton aria-label="previous day" rounded={"full"} size={{ base: "sm", md: "md" }} icon={<Icon as={FiArrowLeft} color={"black"} />} />
+            <IconButton aria-label="next day" rounded={"full"} size={{ base: "sm", md: "md" }} icon={<Icon as={FiArrowRight} color={"black"} />} />
+          </ButtonGroup>
+        </Flex>
 
-                <Search initialValue={searchValue} onValueChange={(value) => setSearchValue(value)} />
-            </HStack>
+        <Search initialValue={searchValue} onValueChange={(value) => setSearchValue(value)} />
+      </HStack>
 
-            {!isLoading && tasks ?
-                <DndProvider backend={HTML5Backend}>
-                    <Flex mt={4} gap={4} justifyContent={"space-between"} flexWrap={"wrap"}>
-                        <DropColumn title="To do" tasks={todoTasks} />
-                        <DropColumn title="In progress" tasks={inProgressTasks} />
-                        <DropColumn title="Completed" tasks={completedTasks} />
-                    </Flex>
-                </DndProvider> :
-                <Flex mt={4} justifyContent={"space-between"}>
-                    {new Array(3).fill("").map((_, i) => <Skeleton key={i} w={"30.33%"} h={"50vh"} />)}
-                </Flex>
-            }
-        </Box>
-    )
-}
+      {!isLoading && tasks ? (
+        <DndProvider backend={HTML5Backend}>
+          <Flex mt={4} gap={4} justifyContent={"space-between"} maxW={{ base: "full" }} overflowX={"scroll"}>
+            <DropColumn title="To do" tasks={todoTasks} />
+            <DropColumn title="In progress" tasks={inProgressTasks} />
+            <DropColumn title="Completed" tasks={completedTasks} />
+          </Flex>
+        </DndProvider>
+      ) : (
+        <Flex mt={4} justifyContent={"space-between"}>
+          {new Array(3).fill("").map((_, i) => (
+            <Skeleton key={i} w={"30.33%"} h={"50vh"} />
+          ))}
+        </Flex>
+      )}
+    </Box>
+  );
+};
